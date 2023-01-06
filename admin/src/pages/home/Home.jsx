8f98d767b930a69ca7e107137f178dbf60609481
Alexisfine <89954228+Alexisfine@users.noreply.github.com>
2023-01-06 14:16:57 -0500

    Added admin sidebar, navbar, widget, and progress bar

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './Home.scss';
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar';
+import Widget from '../../components/widget/Widget';
+import FeaturedCharts from '../../components/featuredCharts/FeaturedCharts';
+import Charts from '../../components/charts/Charts';
 
 const Home = () => {
   return (
     <div className='home'>
-        <div className="title">Hello</div>
+        <Sidebar/>
+        <div className="homeContainer">
+            <Navbar/>
+            <div className="widgets">
+                <Widget type='user'/>
+                <Widget type='order'/>
+                <Widget type='earning'/>
+                <Widget type='balance'/>
+            </div>
+            <div className="charts">
+                <FeaturedCharts/>
+                <Charts/>
+            </div>
+        </div>
     </div>
   )
 }
